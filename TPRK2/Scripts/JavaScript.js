c4f5d702ab5533d3f267ca40e7751e22e7773eec
unknown <y.belyaeva@CORP10.abbyy.ua>
2014-11-28 16:42:08 +0200

    First commit.

---
+++ TPRK2/Scripts/JavaScript.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#buttonPlay").click(function () {
+        var val = $('#inputx').val();
+        if (isNumeric(val)) {     
+        }
+        else {
+
+            alert('not number');
+        }
+    });
+});
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
